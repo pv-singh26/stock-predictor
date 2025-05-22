0995bc8d2d5580c5088cd8e2463422a8dda515d1
pv-singh26 <pvsingh1074@gmail.com>
2025-05-23 01:53:11 +0530

    Add files via upload

--- stock.py
+++ stock.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pandas_datareader as data
+
 import yfinance as yf
 from keras.models import load_model
 import streamlit as st
