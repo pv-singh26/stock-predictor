c64785b63784a0f1d974be817e6bf4392625c177
pv-singh26 <pvsingh1074@gmail.com>
2025-05-23 03:48:14 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -30,78 +30,16 @@ plt.plot(ma100)
 plt.plot(df.Close)
 st.pyplot(fig)
 
-st.subheader('Closing Price vs Time Chart with 100MA & 200MA')
+st.subheader('Prediction vs Original')
 ma100 = df.Close.rolling(100).mean()
 ma200 = df.Close.rolling(200).mean()
 fig = plt.figure(figsize=(12,6))
-plt.plot(ma100, 'g')
-plt.plot(ma200, 'r')
-plt.plot(df.Close, 'b')
-st.pyplot(fig)
-
-data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
-data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
-
-
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(0,1))
-data_training_array = scaler.fit_transform(data_training)  
-
-x_train = []
-y_train = []
-
-for i in range(100,data_training_array.shape[0]):
-    x_train.append(data_training_array[i-100:i])
-    y_train.append(data_training_array[i, 0])
-x_train, y_train= np.array(x_train), np.array(y_train) 
-
-from keras.layers import Dense, Dropout, LSTM
-from keras.models import Sequential
-
-model = Sequential()
-
-model.add(LSTM(units = 50, activation ='relu', return_sequences = True, input_shape= (x_train.shape[1], 1)))
-model.add(Dropout(0.2))
-
-model.add(LSTM(units = 60, activation ='relu', return_sequences = True))
-model.add(Dropout(0.3))
-
-model.add(LSTM(units = 80, activation ='relu', return_sequences = True))
-model.add(Dropout(0.4))
-
-model.add(LSTM(units = 120, activation ='relu'))
-model.add(Dropout(0.5))
-
-model.add(Dense(units=1))
-
-model.compile(optimizer ='adam', loss= 'mean_squared_error')
-model.fit(x_train, y_train, epochs = 3)
-
-past_100_days = data_training.tail(100)
-
-final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
-
-input_data= scaler.fit_transform(final_df)
-
-x_test=[]
-y_test=[]
-for i in range(100,input_data.shape[0]):
-    x_test.append(input_data[i-100:i])
-    y_test.append(input_data[i, 0])
-x_test, y_test= np.array(x_test), np.array(y_test)
-
-y_predicted = model.predict(x_test)
-scaler= scaler.scale_
-
-scale_factor = 1/scaler[0]
-y_predicted= y_predicted*scale_factor
-y_test= y_test*scale_factor
-
-st.subheader('Predictions vs Original')
-fig2 = plt.figure(figsize=(12,6))
-plt.plot(y_test, 'b', label= 'Original Price')
-plt.plot(y_predicted, 'r', label= 'Predicted Price')
+plt.plot(ma100, 'g', label= 'Original Price')
+plt.plot(ma200, 'r', label= 'Predicted Price')
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.legend()
-st.pyplot(fig2)
+st.pyplot(fig)
+
+
+
